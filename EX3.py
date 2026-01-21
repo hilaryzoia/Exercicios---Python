6480c651d4b8d88a965aa27c3a9ca965007e5c3c
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-21 13:59:29 +0100

    Add predecessor and successor calculation in EX3.py

---
+++ EX3.py
@@ -0,0 +1,6 @@
+# Predecessor and Successor of a number
+
+n = int(input('Digite um número: '))
+a = n - 1
+s = n + 1
+print('Baseado no valor de {}, o antecessor é {} e o sucessor é {}.'.format(n,a,s))
