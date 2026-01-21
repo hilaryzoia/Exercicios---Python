f37b6296dd9a5cf43274d01f9be2ddbce829c7f3
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-21 14:09:26 +0100

    Implement multiplication table in Ex7.py
    Added a multiplication table generator for a user-defined number.

---
+++ Ex7.py
@@ -0,0 +1,39 @@
+# Times tables
+
+n = int(input('Enter a number to see your multiplication table: '))
+t1 = n * 1 
+t2 = n * 2
+t3 = n * 3
+t4 = n * 4
+t5 = n * 5
+t6 = n * 6
+t7 = n * 7
+t8 = n * 8
+t9 = n * 9
+t10 = n * 10
+print('====================')
+print('6 x 1 = {}'.format(t1))
+print('6 x 2 = {}'.format(t2))
+print('6 x 3 = {}'.format(t3))
+print('6 x 4 = {}'.format(t4))
+print('6 x 5 = {}'.format(t5))
+print('6 x 6 = {}'.format(t6))
+print('6 x 7 = {}'.format(t7))
+print('6 x 8 = {}'.format(t8))
+print('6 x 9 = {}'.format(t9))
+print('6 x 10 = {}'.format(t10))
+print('====================')
+
+'''
+To make the code shorter, I can replace it with 
+
+n = int(input('Enter a number to see your multiplication table: '))
+print('{} * {} = {}'.format(n,1,n*1))
+
+'''
+'''
+ When I put print('-' * 12) it puts this symbol 12 times on the screen 
+ Ex: ------------
+'''
+
+
