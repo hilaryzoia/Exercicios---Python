7b5fca82aeff1b1827772a988cdde77c5b74cb3a
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-21 14:02:33 +0100

    Add grade point average calculation in EX5.py

---
+++ EX5.py
@@ -0,0 +1,6 @@
+# A student's grade point average
+
+n1 = float(input('First note: '))
+n2 = float(input('Second note: '))
+m = (n1+n2)/2
+print('The average between the grades {:.1f} and {:.1f} is {:.1f}'.format(n1,n2,m))
