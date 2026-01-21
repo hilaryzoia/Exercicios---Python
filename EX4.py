b781dd603e4e50410d2831149e3e30725d53b53e
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-21 14:00:26 +0100

    Add calculations for double, triple, and square root

---
+++ EX4.py
@@ -0,0 +1,7 @@
+# The double, triple and root of a number
+
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** 2
+print('O dobro de {} é {}, \nO triplo de é {}, \nA raiz quadrada é {}.'.format(n,d,t,r))
