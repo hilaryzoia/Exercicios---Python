d92c7feece1fe88cf8de2de67651c20fbac34878
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-21 14:05:50 +0100

    Translate input prompt and output message to English

--- EX3.py
+++ EX3.py
@@ -1,6 +1,6 @@
 # Predecessor and Successor of a number
 
-n = int(input('Digite um número: '))
+n = int(input('Enter a number: '))
 a = n - 1
 s = n + 1
-print('Baseado no valor de {}, o antecessor é {} e o sucessor é {}.'.format(n,a,s))
+print('Based on the value of {}, the predecessor is {} and the successor is {}.'.format(n,a,s))
