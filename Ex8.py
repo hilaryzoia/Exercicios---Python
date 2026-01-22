0d53957d99d89b9e89317a424d92ae70b4182302
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-22 17:26:28 +0100

    Fix typo in currency output format

--- Ex8.py
+++ Ex8.py
@@ -2,4 +2,4 @@
 
 n = float(input('How much money do you have in your wallet?'))
 d = n / 5.29
-print('With R${:.2f} you can buy U${:.2f}.'.format(n,d))
+print('With R${:.2f} you can buy US${:.2f}.'.format(n,d))
