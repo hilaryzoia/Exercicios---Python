660edf39fe7a47f43e0a9d52be082acb6a0789c8
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-01-29 18:43:10 +0100

    Add text analyzer for name analysis
    Fix variable name typo in printing first name.

---
+++ Ex20.py
@@ -0,0 +1,9 @@
+#Text analyzer
+
+name = str(input('Enter your full name: '))
+print('Analyzing your name...')
+print('Your name in capital letters is {}'.format(name.upper()))
+print('Your name in lowercase is {}'.format(name.lower()))
+print('Your name has a total of {} letters'.format(len(name)-name.count(' ')))
+separate = name.split()
+print('His first name is {} and he has {} letters'.format(separa[0],len(separa[0])))
