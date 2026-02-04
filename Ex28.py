fee5ac33b31ef06877dcc70a0c400ff0fc87e196
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-02-04 15:50:27 +0100

    Add odd or even number checker

---
+++ Ex28.py
@@ -0,0 +1,8 @@
+#Odd or even
+
+number = int(input('Tell me any number: '))
+result = number % 2
+if result == 0:
+    print('The number {} is EVEN'.format(number))
+else:
+    print('The number {} is ODD'.format(number))
