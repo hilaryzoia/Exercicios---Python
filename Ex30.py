70968a30a850243ffd5dfc2e44c7955795b03e0d
hilaryzoia <163075751+hilaryzoia@users.noreply.github.com>
2026-02-04 16:30:18 +0100

    Add leap year analysis functionality

---
+++ Ex30.py
@@ -0,0 +1,10 @@
+#Leap year
+
+from datetime import date
+year = int(input('What year to analyze (put 0 for the current year)? '))
+if year == 0:
+    year = date.today().year
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print('The year {} is a leap year'.format(year))
+else:
+    print('The year {} is not a leap year'.format(year))
